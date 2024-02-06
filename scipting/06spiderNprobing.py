f53a4eb733c09e6385c0119bb334400c89318802
Fazil S <fazil.s@experionglobal.local>
2024-02-06 11:38:28 +0530

    completed kb scrapping script

--- scipting/06spiderNprobing.py
+++ scipting/06spiderNprobing.py
@@ -5,95 +5,90 @@ import requests
 import time
 import re
 
-url = "https://msrc.microsoft.com/update-guide/vulnerability/CVE-2022-24493"
 
-# Set up a browser with Selenium
-options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
+while( True):
+    url = input("enter the Url: ")
 
-try:
-    driver = webdriver.Chrome(options=options)  # Updated line
+    options = webdriver.ChromeOptions()
 
-    # Make the initial request
-    driver.get(url)
+    try:
+        driver = webdriver.Chrome(options=options)  
 
-    # Allow some time for JavaScript to load content
-    time.sleep(10)
+        driver.get(url)
 
-    # Get the page source after JavaScript execution
-    html_source = driver.page_source
+        time.sleep(10)
 
-    # Parse the HTML content
-    soup = BeautifulSoup(html_source, 'html.parser')
+        html_source = driver.page_source
 
-    # Find all anchor tags
-    atags = soup.find_all("a")
-    server2019 = soup.find('div' ,string="Windows Server 2019")
-    # print(server2019.parent)
-    
-    KB2019 = server2019.parent.find("a")
-    print(f"the a tag is {KB2019}")
+        soup = BeautifulSoup(html_source, 'html.parser')
 
-    KB2019link = KB2019.get("href")
-    print(f"the link is {KB2019link}")
-
-    print(KB2019.text)
-
-    # Iterate through anchor tags and print href attributes
-    # for i in atags:
-    #     href = i.get("href")
-    #     if href is not None:
-    #         if "support" in href:
-    #             print(href) 
-
-    # Print HTML source before closing the browser
-    # print("HTML source before closing the browser:")
-    # print(driver.page_source) 
-
-
-     # Make the initial request
-    # driver.get(KB2019link)
-
-    # Allow some time for JavaScript to load content
-    # time.sleep(10)
-
-    # Get the page source after JavaScript execution
-    # html_source = driver.page_source
-
-    # Parse the HTML content
-    res = requests.get(KB2019link)
-    if res.status_code == 200:
-        kb2019soup = BeautifulSoup(res.content, 'html.parser')
-    # print(kb2019soup)
-
-        test = kb2019soup.find('p', string="Prerequisite:")
-        next_sibling = test.find_next_sibling()
-    # print(next_sibling)
-        ssufor2019 = next_sibling.find("a").text
-
-        print(f"the ssu for 2019 server is {ssufor2019}")
-
-
+        # Find all anchor tags
+        atags = soup.find_all("a")
+        server2019 = soup.find('div' ,string="Windows Server 2019")
+        server2022 = soup.find('div' ,string="Windows Server 2022")
         
-        lcuwithjunk = kb2019soup.find('h1')
-        lcutext = lcuwithjunk.text
+        KB2019 = server2019.parent.find("a")
+        # print(f"the a tag is {KB2019}")
 
-        kb_match = re.search(r'KB\d+', lcutext)
-        # kb_number = "test"
-        if kb_match:
-            kb_number = kb_match.group()
-            print(kb_number)
-        print(f"install the SSU {ssufor2019} then install LCU {kb_number}")
+        KB2019link = KB2019.get("href")
+        print(f"the link for 2019 : {KB2019link}")
 
+        # print(KB2019.text)
 
+        KB2022 = server2022.parent.find("a")
+        # print(f"the a tag is {KB2022}")
 
+        KB2022link = KB2022.get("href")
+        print(f"the link for 2022 :  {KB2022link}")
 
-    
+        # print(KB2022.text)
 
-except WebDriverException as e:
-    print(f"WebDriver error: {e}")
+    # for 2019 server 
+        # Parse the HTML content
+        res = requests.get(KB2019link)
+        if res.status_code == 200:
+            kb2019soup = BeautifulSoup(res.content, 'html.parser')
 
-finally:
-    # Close the browser
-    if 'driver' in locals() and driver is not None:
-        driver.quit()
+            test = kb2019soup.find('p', string="Prerequisite:")
+            next_sibling = test.find_next_sibling()
+            ssufor2019 = next_sibling.find("a").text
+
+            # print(f"the ssu for 2019 server is {ssufor2019}")
+
+            lcuwithjunk = kb2019soup.find('h1')
+            lcutext = lcuwithjunk.text
+
+            kb_match = re.search(r'KB\d+', lcutext)
+            # kb_number = "test"
+            if kb_match:
+                kb_number = kb_match.group()
+                # print(kb_number)
+            print(f"install the SSU {ssufor2019} then install LCU {kb_number}")
+        else:
+            print(res.status_code)
+        
+    #    for server 2022
+
+        res = requests.get(KB2022link)
+        if res.status_code == 200:
+            kb2019soup = BeautifulSoup(res.content, 'html.parser')
+            
+            lcuwithjunk = kb2019soup.find('h1')
+            lcutext = lcuwithjunk.text
+
+            kb_match = re.search(r'KB\d+', lcutext)
+            # kb_number = "test"
+            if kb_match:
+                kb_number = kb_match.group()
+                # print(kb_number)
+            print(f"install the 2022 LCU {kb_number}")
+        else:
+            print(res.status_code)
+            
+    except WebDriverException as e:
+        print(f"WebDriver error: {e}")
+
+    finally:
+        # Close the browser
+        if 'driver' in locals() and driver is not None:
+            driver.quit()
